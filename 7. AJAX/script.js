1670aaa31c0306954919456182d2be2d1ea55bb5
StrangerWS <strangernf@gmail.com>
2017-09-27 12:38:23 +0300

    Patch

--- 7. AJAX/script.js
+++ 7. AJAX/script.js
@@ -1,69 +1,78 @@
-//use methods from + q
+var loadDataModule = (function () {
+    var from,
+        searchText;
+
+    return {
+        loadData: function (params) {
+            $.ajax({
+                type: 'GET',
+                url: 'http://demo.webility.ru/api',
+                data: params,
+                dataType: 'json',
+                success: function (data) {
+                    var $nextBtn = $('#next-btn');
+                    var $prevBtn = $('#prev-btn');
+
+                    from = data.meta.from;
+                    length = data.meta.length;
+                    loadDataModule.drawMeta();
+                    loadDataModule.drawData(data.data);
+
+                    if (from + 10 > length) {
+                        $nextBtn.hide();
+                    }
+                    else {
+                        $nextBtn.show();
+                    }
+
+                    if (from === 0) {
+                        $prevBtn.hide();
+                    }
+                    else {
+                        $prevBtn.show();
+                    }
+
+                }
+            })
+        },
+
+        drawMeta: function () {
+            $('#start-num').text(length === 0 ? 0 : from + 1);
+            $('#size').text(length);
+            $('#end-num').text(from + 9 <= length ? from + 10 : length);
+        },
+
+        drawData: function (data) {
+
+            var $table = $('#table');
+            $table.empty();
+            var tableContent;
+            for (var i = 0; i < data.length; i++) {
+                tableContent += '<tr><td>' + data[i].name + '</td><td>' + data[i].episodes + '</td></tr>';
+            }
+            $table.append(tableContent);
+        },
+
+        nextPage: function () {
+            alert("from=" + (from + 10) + "&" + loadDataModule.searchText);
+            loadDataModule.loadData("from=" + (from + 10) + "&" + searchText);
+        },
+
+        prevPage: function () {
+            loadDataModule.loadData("from=" + (from - 10) + "&" + searchText);
+        },
+
+        search: function () {
+            searchText = "q=" + $('#search-input').val();
+            loadDataModule.loadData(searchText);
+        }
+    }
+}());
+
 $(document).ready(function () {
-    loadData();
+    loadDataModule.loadData();
 });
 
-
-function loadData(params) {
-    $.ajax({
-        type: 'GET',
-        url: 'http://demo.webility.ru/api',
-        data: params,
-        dataType: 'json',
-        success: function (data) {
-            var $nextBtn = $('#next-btn');
-            var $prevBtn = $('#prev-btn');
-
-            from = data.meta.from;
-            length = data.meta.length;
-            drawMeta();
-            drawData(data.data);
-
-            if (from + 10 > length) {
-                $nextBtn.hide();
-            }
-            else {
-                $nextBtn.show();
-            }
-
-            if (from === 0) {
-                $prevBtn.hide();
-            }
-            else {
-                $prevBtn.show();
-            }
-
-        }
-    })
-}
-
-function drawMeta() {
-    $('#start-num').text(length === 0 ? 0:from + 1);
-    $('#size').text(length);
-    $('#end-num').text(from + 9 <= length ? from + 10 : length);
-}
-
-function drawData(data) {
-
-    var $table = $('#table');
-    $table.empty();
-    var tableContent;
-    for (var i = 0; i < data.length; i++) {
-        tableContent += '<tr><td>' + data[i].name + '</td><td>' + data[i].episodes + '</td></tr>';
-    }
-    $table.append(tableContent);
-}
-
-function nextPage() {
-    alert("from=" + (from + 10) + "&" + searchText);
-    loadData("from=" + (from + 10) + "&" + searchText);
-}
-
-function prevPage() {
-    loadData("from=" + (from - 10) + "&" + searchText);
-}
-
-function search() {
-    searchText =  "q=" + $('#search-input').val();
-    loadData(searchText);
-}
+$('#next-btn').click(loadDataModule.nextPage());
+$('#prev-btn').click(loadDataModule.prevPage());
+$('#search-btn').click(loadDataModule.search());
