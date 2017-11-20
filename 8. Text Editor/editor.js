38c0a384f21561a8d438ca3ca6cdbf2fd019aba8
StrangerWS <strangernf@gmail.com>
2017-11-20 15:35:43 +0400

    Text Editor: Decorations     + Alignment       + Undo/Redo       + Cut             + Copy            + Paste           + Paste as text   + Pictures        + Table           + JSON            + Print           - API             +

--- 8. Text Editor/editor.js
+++ 8. Text Editor/editor.js
@@ -3,22 +3,36 @@
     let $boldBtn = $('#bold-btn'),
         $italicBtn = $('#italic-btn'),
         $underlineBtn = $('#underline-btn'),
+
         $forwardBtn = $('#forward-btn'),
         $backBtn = $('#back-btn'),
         $backDropdown = $('#back-dropdown'),
         $fwdDropdown = $('#fwd-dropdown'),
+
         $textArea = $('#text-area'),
+
         $leftAlignBtn = $('#left-align-btn'),
         $rightAlignBtn = $('#right-align-btn'),
         $centerAlignBtn = $('#center-align-btn'),
+
         $cutDropdown = $('#cut-dropdown'),
         $copyDropdown = $('#copy-dropdown'),
         $pasteDropdown = $('#paste-dropdown'),
         $pasteTextDropdown = $('#paste-text-dropdown'),
+
+        $importDropdown = $('#import-dropdown'),
+        $exportDropdown = $('#export-dropdown'),
+        $printDropdown = $('#print-dropdown'),
+
         $imgBtn = $("#img-btn"),
         $tableBtn = $('#table-btn'),
+
+        $modalImport = $('#modal-import'),
+        $modalImportInput = $('#import-input'),
+
         $apiBtn = $('#api-btn'),
         $apiDiv = $('#api-div'),
+
         currentIndex = (localStorage.getItem("curIdx") === null) ? 0 : localStorage.getItem("curIdx"),
         buffer,
         selection,
@@ -41,9 +55,41 @@
             }
         },
 
+        exportJSON = () => {
+            let fileData = {
+                date: new Date(),
+                text: $textArea.html()
+            };
+
+            let link = document.createElement('a');
+            let file = new File([JSON.stringify(fileData)], {'type': 'json'});
+            link.href = window.URL.createObjectURL(file);
+            link.download = 'text.json';
+            link.click();
+        },
+
+        importJSON = () => {
+            let fr = new FileReader();
+            $modalImport.modal('show');
+            $modalImportInput.on('change', ((e) => {
+                fr.onload = (event) =>{
+                    $textArea.html(JSON.parse(event.target.result).text);
+                    saveInHistory();
+                    $modalImport.modal('hide');
+                };
+
+                let files = e.target.files;
+                if (files.length === 1) {
+                    let file = files[0];
+                    fr.readAsText(file);
+                }
+            }));
+
+
+        },
+
         execCmd = (decorator, ui, value) => {
             document.execCommand(decorator, ui, value);
-            saveInHistory();
         },
 
         loadText = () => {
@@ -121,26 +167,38 @@
             $('#' + title).on('click', new Function("", func));
         };
 
+    document.onkeydown = function (e) {
+        if (e.ctrlKey && e.keyCode === 86) {
+            saveInHistory();
+        }
+    };
+
     return {
         init: () => {
             loadText();
             $boldBtn.click(() => {
                 execCmd("bold");
+                saveInHistory();
             });
             $italicBtn.click(() => {
                 execCmd("italic");
+                saveInHistory();
             });
             $underlineBtn.click(() => {
                 execCmd("underline");
+                saveInHistory();
             });
             $leftAlignBtn.click(() => {
                 execCmd("justifyLeft");
+                saveInHistory();
             });
             $rightAlignBtn.click(() => {
                 execCmd("justifyRight");
+                saveInHistory();
             });
             $centerAlignBtn.click(() => {
                 execCmd("justifyCenter");
+                saveInHistory();
             });
             $backBtn.click(() => {
                 historyIteration(false);
@@ -152,29 +210,36 @@
                 historyIteration(true);
             });
             $fwdDropdown.click(() => {
-                //execCmd("redo");
                 historyIteration(true);
             });
             $cutDropdown.click(() => {
                 execCmd("cut");
+                saveInHistory();
             });
             $copyDropdown.click(() => {
                 execCmd("copy");
+                saveInHistory();
             });
             $pasteDropdown.click(() => {
                 paste();
             });
             $pasteTextDropdown.click(() => {
                 pasteText();
+                saveInHistory();
             });
-            $pasteTextDropdown.click(() => {
-                execCmd("insertText");
+            $exportDropdown.click(() => {
+                exportJSON();
+            });
+            $importDropdown.click(() => {
+                importJSON();
             });
             $imgBtn.click(() => {
                 insertImg();
+                saveInHistory();
             });
             $tableBtn.click(() => {
                 addTable();
+                saveInHistory();
             });
             $apiBtn.click(() => {
                 addControl();
